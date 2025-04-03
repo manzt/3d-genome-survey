c3b53963f6e6c47a9c87955053762e13862c3dd5
Trevor Manz <trevor.j.manz@gmail.com>
2025-04-03 12:56:35 -0400

    Setup tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,12 @@
 import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
 
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
-  integrations: [svelte()]
+  integrations: [svelte()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
