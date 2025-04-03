927ff1a432576e28d47d7ff83aa5a438696c107b
Trevor Manz <trevor.j.manz@gmail.com>
2025-04-03 14:11:54 -0400

    Init content config

---
+++ src/content.config.ts
@@ -0,0 +1,76 @@
+import { z, defineCollection, reference } from "astro:content";
+
+const sourceCollection = defineCollection({
+	type: "data",
+	schema: z.object({
+		path: z.string(),
+		guid: z.string(),
+		name: z.string(),
+	}),
+});
+
+const codeCollection = defineCollection({
+	type: "data",
+	schema: z
+		.object({
+			guid: z.string(),
+			name: z.string(),
+			isCodable: z.enum(["true", "false"]),
+		})
+		.transform((obj) => {
+			return {
+				...obj,
+				isCodable: obj.isCodable === "true",
+			};
+		}),
+});
+
+const quoteCollection = defineCollection({
+	type: "data",
+	schema: z.object({
+		source_guid: reference("sources"),
+		subfig_num: z.string().nullable().optional(),
+		attrs: z.object({
+			guid: z.string(),
+			page: z.string(),
+		}),
+		Coding: z.array(
+			z.object({
+				attrs: z.object({
+					guid: z.string(),
+				}),
+				CodeRef: z.object({
+					attrs: z.object({
+						targetGUID: reference("codes"),
+					}),
+				}),
+			}),
+		),
+	}),
+});
+
+const codeSetCollection = defineCollection({
+	type: "data",
+	schema: z.object({
+		attrs: z.object({
+			guid: z.string(),
+			name: z.string(),
+		}),
+		MemberCode: z.array(
+			z.object({
+				attrs: z.object({
+					targetGUID: reference("codes"),
+				}),
+			}),
+		),
+	}),
+});
+
+// 3. Export a single `collections` object to register your collection(s)
+//    This key should match your collection directory name in "src/content"
+export const collections = {
+	codes: codeCollection,
+	quotations: quoteCollection,
+	sources: sourceCollection,
+	//'sets': codeSetCollection,
+};
